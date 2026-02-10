08b002b29477335b8aa62c36038f7b50b026e3c6
Ben Joe Mathews <joemathews4development@gmail.com>
2026-02-10 17:55:07 +0100

    completed all iterations

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 
 function ProductDetailsPage() {
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
   const [product, setProduct] = useState({});
+  const params = useParams()
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${params.productId}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((response) => {
+        console.log(response)
+        setProduct(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
 
-  // The `productId` coming from the URL parameter is available in the URL path.
-  // You can access it with the `useParams` hook from react-router-dom.
-
-
-  // To fetch the product details, set up an effect with the `useEffect` hook:
+  const handleBackButtonClick = (() => {
+    navigate("/")
+  })
 
 
 
   return (
     <div className="ProductDetailsPage">
-    {/* Render product details here */}
+      {/* Render product details here */}
+      <br />
+      <div className="Product-details-container">
+        <div className="Product-details">
+          <img src={product.image} alt="product" />
+          <p className="category">{product.category}</p>
+          <p className="title">{product.title}</p>
+          <div className="product-description">
+            <p>{product.description}</p>
+            <p className="price">${product.price}</p>
+          </div>
+        </div>
+        <hr />
+        <button className="backButton" onClick={handleBackButtonClick}>Back</button>
+      </div>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 function ProductListPage() {
@@ -7,13 +8,44 @@ function ProductListPage() {
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
-
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => {
+      console.log(response)
+      setProducts(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      <br />
+      {products.map((product) => {
+        return(
+          <Link to={`/product/details/${product.id}`} className="product-list-row" key={product.id}>
+            <img src={product.image} alt="product" />
+            <p className="title">{product.title}</p>
+            <p>{product.category}</p>
+            <p>${product.price}</p>
+            <p>{product.description}</p>
+          </Link>
+        )
+      })}
     </div>
   );
 }
 
 export default ProductListPage;
+/*
+category: "men's clothing"
+description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+id: 1
+image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
+price: 109.95
+rating: {rate: 3.9, count: 120}
+title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"*/
